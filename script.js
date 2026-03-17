32d2febeb26cc764da037172d904417a79c82b1d
Marjana731 <marjanauk731@gmail.com>
2026-03-17 17:52:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const APIkey = "511bee3d97901acd6df247cc9fa3d6d7";
-const url = "https://api.themoviedb.org/3/movie/now_plaiyng?api_key=${API_KEY}&language=uk-UA&page1";
+const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${APIkey}&language=uk-UA&page=1`;
 
 fetch(url)
 .then(response => response.json())
 .then(data => {
     const moviesConteiner = document.getElementById("movies");
     data.results.forEach(movie => {
-        const poster = "https://image.tmbd.org/t/p/w500" + movie.poster_path;
+        const poster = "https://image.tmbb.org/t/p/w500" + movie.poster_path;
         const card = document.createElement("a");
         card.href = "https://www.themoviedb.org/movie/" + movie.id;
         card.target = "_blank";
 
-        card.innerHTML = '
+        card.innerHTML = `
         <div class="movie-card">
             <img src="${poster}">
             <p>${movie.title}</p>
-        </div>';
+        </div>`;
         moviesConteiner.appendChild(card)
     });
 });
